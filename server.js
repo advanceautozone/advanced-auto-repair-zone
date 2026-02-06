835f217368601527fbb4ff9309a2b517d2b0e2ce
advanceautozone <advanceautozone25@gmail.com>
2026-02-07 01:55:00 +0600

    update localhost to 0.0.0.0

--- server.js
+++ server.js
@@ -731,7 +731,7 @@ app.use((err, req, res, next) => {
 
 // ========== START SERVER ==========
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log('');
     console.log('='.repeat(50));
     console.log('🚀 AUTO SHOP BACKEND SERVER');
